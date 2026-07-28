fda17b89906d099354b168f66d02b0339ec4a53f
Bikash Bisoyi <bisoyi2006@gmail.com>
2026-07-28 16:35:00 +0530

    Day5

---
+++ C++/operator.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // cout<<13.2*4;
+    // int a=10;
+    // int b=a++;
+    // cout<<b<<" "<<a;
+
+    // Comparison Opertaor
+    // int a,b;
+    // cout<<"Enter the value of a: ";
+    // cin>>a;
+    // cout<<"Enter the value of b: ";
+    // cin>>b;
+
+    // ==
+
+    // if(a==b)
+    // cout<<"yes";
+    // else
+    // cout<<"No";
+
+    // >,<,>=,<=,!=
+
+    // if(a<b)
+    // cout<<"yes";
+    // else
+    // cout<<"No";
+
+    // Logical operator
+    // int a,b,c;
+    // cout<<"Enter the value of a: ";
+    // cin>>a;
+    // cout<<"Enter the value of b: ";
+    // cin>>b;
+    // cout<<"Enter the value of c: ";
+    // cin>>c;
+
+    // if(a>b && a>c)
+    // cout<<"yes";
+    // else
+    // cout<<"No";
+    
+
+    // Bitwise Operator
+
+    //  &, |, >>, << Operator
+    int ans = 6<<1;
+    cout<<"My ans is: "<<ans<<endl;
+
+}
---
+++ C++/whileloop.cpp
@@ -0,0 +1,41 @@
+#include<iostream> 
+using namespace std; 
+int main(){ 
+    // int n; 
+    // cout<<"Enter table no.: "; 
+    // cin>>n; 
+    // int i=1; 
+    // while(i<=10){
+    //      cout<<n*i<<endl; 
+    //      i++; 
+    //     } 
+
+    // Do while
+    // int n;
+    // cout<<"Enter the Number: ";
+    // cin>>n;
+    // int i=1;
+    // do{
+    //     cout<<i<<" ";
+    //     i++;
+    // }while(i<=n);
+
+    // Switch
+    int i;
+    cout<<"Enter your choice";
+    cin>>i;
+
+    switch(i){
+
+        case 1:
+        cout<<"Bikash";
+        break;
+        
+        case 2:
+        cout<<"Rakesh";
+        break;
+        
+        default:
+        cout<<"Hii";
+    }
+    }
